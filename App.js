825038f18b903957f94e42f5cd6ae845757ad1e8
botdenoms <dudubot404@gmail.com>
2022-10-17 13:57:48 +0300

    mock homepage ui

--- App.js
+++ App.js
@@ -6,19 +6,41 @@ import Home from './src/screens/Home'
 import Registration from './src/screens/Registration'
 import Validation from './src/screens/Validation'
 
+import {Bars3Icon, ChevronLeftIcon, UserPlusIcon, PencilSquareIcon} from "react-native-heroicons/solid";
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import {
   DrawerContentScrollView,
-  DrawerItem
+  // DrawerItem
 } from '@react-navigation/drawer';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native'
 
 function CustomDrawerContent(props) {
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItem
+    <DrawerContentScrollView {...props} style={styles.drawer}>
+      <View>
+        <TouchableOpacity onPress={()=> props.navigation.closeDrawer()} style={styles.btns}>
+          <ChevronLeftIcon color="#1e1e1e" size={24}/>
+        </TouchableOpacity>
+          <Text style={styles.titleText}>Services</Text>
+        <TouchableOpacity onPress={()=> props.parent.navigate('Registration')} style={styles.btns}>
+          <UserPlusIcon color="#1e1e1e" size={24}/>
+          <Text style={styles.linkText}>Voter Registration</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=> props.parent.navigate('Validation')} style={styles.btns}>
+          <PencilSquareIcon color="#1e1e1e" size={24}/>
+          <Text style={styles.linkText}>Voter Records</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <DrawerItem
         label="Close"
         onPress={() => props.navigation.closeDrawer()}
       />
@@ -29,7 +51,7 @@ function CustomDrawerContent(props) {
       <DrawerItem
         label="Voter Records"
         onPress={() => props.parent.navigate('Validation')}
-      />
+      /> */}
     </DrawerContentScrollView>
   );
 }
@@ -40,17 +62,18 @@ const Drawer = createDrawerNavigator();
 function HomeScreen({navigation}) {
   return (
     <Drawer.Navigator 
-        drawerContent={(props) => <CustomDrawerContent {...props} parent={navigation} />}
-        screenOptions={{
-          headerShown: true,
-          headerTitle: 'OurVotes',
-          headerTitleAlign: 'center',
-          headerStyle: {
-            elevation: 0,
-          }
-        }}
+      drawerContent={(props) => <CustomDrawerContent {...props} parent={navigation} />}
+      screenOptions={{
+        headerShown: true,
+        headerTitle: 'OurVotes',
+        headerTitleAlign: 'center',
+        headerStyle: {
+          elevation: 0,
+        },
+        drawerIcon: ({ focused, color, size})=> <Bars3Icon size={size} color='#1e1e1e'/>
+      }}
       >
-      <Drawer.Screen name="main" component={Home} />
+      <Drawer.Screen name="main" component={Home}/>
     </Drawer.Navigator>
   );
 }
@@ -65,11 +88,34 @@ const App = () => {
           component={HomeScreen}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Registration" component={Registration } />
-        <Stack.Screen name="Validation" component={Validation} />
+        <Stack.Screen name="Registration" component={Registration} options={{ headerShown: false }}/>
+        <Stack.Screen name="Validation" component={Validation} options={{ headerShown: false }}/>
 	    </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  drawer:{
+    backgroundColor: '#d9d9d9',
+    flex: 1,
+    height: '100%'
+  },
+  titleText:{
+    paddingHorizontal: 10,
+    marginVertical: 5,
+  },
+  linkText:{
+    fontSize: 17,
+    color: '#1e1e1e',
+    paddingHorizontal: 10,
+  },
+  btns:{
+    paddingHorizontal: 10,
+    marginVertical: 5,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+});
 
 export default App;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,38 @@
 import { 
-    View, 
-    Text, 
-    SafeAreaView
+  View,
+  Text,
+  StyleSheet,
+  ScrollView
 } from 'react-native'
+
+import ElectionCard from '../components/ElectionCard';
+
 import React from 'react'
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Home</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.body}>
+        <Text style={styles.title}>on going</Text>
+        <ElectionCard active={true}/>
+        <Text style={styles.title}> Previous</Text>
+        <ScrollView>
+          <ElectionCard active={false}/>
+          <ElectionCard active={false}/>
+          <ElectionCard active={false}/>
+          <ElectionCard active={false}/>
+        </ScrollView>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title:{
+    fontSize: 17,
+    color: '#1e1e1e'
+  },
+  body:{
+    paddingHorizontal: 10,
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
